5f033dc96949d571c9c05181faf49441bac2410a
Daniel Aquino <daniel.j.aquino@gmail.com>
2016-04-08 17:29:12 -0500

    Finished chapter 5

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -24,6 +24,29 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
         nf.maximumFractionDigits = 1
         return nf
     }()
+
+    let darkBackgroundColor = UIColor.grayColor();
+    let lightBackgroundColor = UIColor(red: 0.961, green: 0.957, blue: 0.945, alpha: 1.00);
+    let dateFormat: NSDateFormatter = {
+        let df = NSDateFormatter()
+        df.dateFormat = "HH"
+        return df
+    }()
+    
+    override func viewDidLoad() {
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        let currentHour = NSInteger(dateFormat.stringFromDate(NSDate()));
+        if let hour = currentHour {
+            if hour >= 17 || hour <= 5 {
+                view.backgroundColor = self.darkBackgroundColor
+            }
+            else {
+                view.backgroundColor = self.lightBackgroundColor
+            }
+        }
+    }
     
     @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
         if let text = textField.text, value = Double(text) {
